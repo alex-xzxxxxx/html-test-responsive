663419caf4f3fd260225c5335f1ff2828f4f49d4
alex-xzxxxxx <alex.xzxxxxx@gmail.com>
2025-01-21 12:13:42 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'html-test';
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      rememberMe: [false],
+    });
+  }
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const formValues = this.loginForm.value;
+      console.log('Form Submitted', formValues); 
+    }
+  }
+}
